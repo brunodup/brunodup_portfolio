4a7e5943818ce5f08ff3276b93fa761e041c3362
Bruno Dup <hi@brunodup.com>
2021-11-25 09:36:30 -0300

    site verification

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
           <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;300;400;700;900&display=swap" rel="stylesheet"></link>
+          <meta name="google-site-verification" content="AMLY2S1nHk0pslY-VD67snNZzT20ZDoT053L_3V0DgM"/>
         </Head>
         <body>
           <Main />
