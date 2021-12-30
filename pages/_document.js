68def0dd64fbbb8033bda15b650a67401cecdc4f
Bruno Dup <hi@brunodup.com>
2021-12-30 19:12:26 -0300

    update seo text

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com"></link>
           <link rel="shortcut icon" href="/favicon.png" />
+          <link rel="canonical" href="https://brunodup.com" />
           <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;300;400;700;900&display=swap" rel="stylesheet"></link>
           <title>Bruno Dup - Faça sua loja virtual, rápida e de qualidade</title>
           <meta name="description" content="Desenvolva sua loja virtual, ou tenha uma consultoria tecnológica para lojas virtuais com um especialista em Vtex e Shopify."></meta>
